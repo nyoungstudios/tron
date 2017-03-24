f7c86d1617d65a4037d76f0bd2b559f8ef7106fa
mhormati <mthormati@gmail.com>
2017-03-24 19:12:22 -0400

    cleaned up some code

--- nathaniel.py
+++ nathaniel.py
@@ -1,11 +1,6 @@
 
 def findLocationHead(playerNum, grid_size, board_state):
-    i = -1
-    j = -1
-    for row in board_state:
-        i += 1
-        for cell in row:
-            j += 1
-            if playerNum == cell:
+    for i in range(0, grid_size):
+        for j in range(0, grid_size):
+            if playerNum == board_state[i][j]:
                 return [i, j]
-        j = -1
