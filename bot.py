de4779da4f5e718c219c1802f533d72bc1ac2e55
nyoungstudios <nathanielyoung95070@gmail.com>
2017-03-24 20:54:57 -0400

    working bot than won a couple battles

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ def select_move(grid_size, board_state, my_player_number, total_player_count):
   currentLocationList = []
   for x in range(int(total_player_count)):
     currentPlayerNum = int(x + 1)
-    print(currentPlayerNum)
+    #print(currentPlayerNum)
     location = n.findLocationHead(str(currentPlayerNum), grid_size, board_state)
     currentLocationList.insert(currentPlayerNum, location)
     #print("this is the pos " + str(currentLocationList[currentPlayerNum-1]) + " This is x:")
@@ -44,10 +44,41 @@ def select_move(grid_size, board_state, my_player_number, total_player_count):
   otherLocation = currentLocationList
   otherLocation.remove(ourCurrentLocation)
 
+  probList = m.playerProb(int(my_player_number), ourCurrentLocation, otherLocation, grid_size, board_state)
+  print(probList)
+
+  currentVal = 0
+  largestVal = 0
+  largestIndex = 0
+  i = 0
+  for y in probList:
+    currentVal = y
+    if i == 0:
+      largestVal = currentVal
+      largestIndex = i
+    elif currentVal > largestVal:
+      largestVal = currentVal
+      largestIndex = i
+
+    i+=1
 
 
-  print_board(board_state) # Print board for debugging
+  print_board(board_state)  # Print board for debugging
   print("")
+
+  if largestIndex == 0:
+    return "UP"
+  elif largestIndex == 1:
+    return "LEFT"
+  elif largestIndex == 2:
+    return "RIGHT"
+  elif largestIndex == 3:
+    return "DOWN"
+
+
+
+
+
 
 
 
